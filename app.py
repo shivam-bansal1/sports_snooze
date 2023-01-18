19a9577ca027e99cadde30bbbd35da6f85ac483e
shivam-bansal1 <84956031+shivam-bansal1@users.noreply.github.com>
2023-01-18 14:17:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,10 @@ import plotly.express as px
 from PIL import Image
 import requests
 import random
-# import json
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# import geopandas
-# import plotly.figure_factory as ff
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.figure_factory as ff
 
-# df = pd.read_csv('./data/athlete_events.csv')
 df = pd.read_csv('./data/athlete_events.zip',compression='zip')
 region_df = pd.read_csv('./data/noc_regions.csv')
 
@@ -45,32 +42,6 @@ if user_menu == 'Medal Tally':
     st.title("Number of Medals by Countries")
     image = Image.open('Medals_by_countries.png')
     st.image(image, caption='Medals by Country')
-
-if user_menu == 'News':
-    st.sidebar.header("News")
-    api_key = '07ce6431517e45c5b04b589c36e5bed6'
-    url = f"https://newsapi.org/v2/everything?q=olympics&apiKey={api_key}"
-    response = requests.get(url)
-    response = response.json()
-    articles = response['articles']
-
-    def get_news() :
-        for article in random.choices(articles) :
-            st.header(article['title'])
-            st.markdown(f"<span style='background-color:blue; padding:10px; border-radius:20px;'>Published at :{article['publishedAt']}</span>",unsafe_allow_html=True)
-            if article['author'] :
-                st.write(article['author'])
-            st.write(article['source']['name'])
-            st.write(article['description'])
-            st.image(article['urlToImage'])
-        return
-
-    for i in range(5) :
-        get_news()
-
-    col1, col2, col3, col4, col5 = st.columns(5)
-    if col3.button('Refresh'):
-        st.experimental_rerun()
 
 if user_menu == 'Overall Analysis':
     st.sidebar.header("Overall Analysis")
@@ -127,3 +98,99 @@ if user_menu == 'Overall Analysis':
     selected_sport = st.selectbox('Select a Sport', sport_list)
     x = helper.most_successful(df, selected_sport)
     st.table(x)
+
+if user_menu == 'Country-wise Analysis':
+
+    st.sidebar.title('Country-wise Analysis')
+
+    country_list = df['region'].dropna().unique().tolist()
+    country_list.sort()
+
+    selected_country = st.sidebar.selectbox('Select a Country',country_list)
+
+    country_df = helper.yearwise_medal_tally(df,selected_country)
+    fig = px.line(country_df, x="Year", y="Medal")
+    st.title(selected_country + " Medal Tally over the years")
+    st.plotly_chart(fig)
+
+    st.title(selected_country + " excels in the following sports")
+    pt = helper.country_event_heatmap(df,selected_country)
+    fig, ax = plt.subplots(figsize=(20, 20))
+    ax = sns.heatmap(pt,annot=True)
+    st.pyplot(fig)
+
+    st.title("Top 10 athletes of " + selected_country)
+    top10_df = helper.most_successful_countrywise(df,selected_country)
+    st.table(top10_df)
+
+if user_menu == 'Athlete wise Analysis':
+    athlete_df = df.drop_duplicates(subset=['Name', 'region'])
+
+    x1 = athlete_df['Age'].dropna()
+    x2 = athlete_df[athlete_df['Medal'] == 'Gold']['Age'].dropna()
+    x3 = athlete_df[athlete_df['Medal'] == 'Silver']['Age'].dropna()
+    x4 = athlete_df[athlete_df['Medal'] == 'Bronze']['Age'].dropna()
+
+    fig = ff.create_distplot([x1, x2, x3, x4], ['Overall Age', 'Gold Medalist', 'Silver Medalist',
+                                                'Bronze Medalist'],show_hist=False, show_rug=False)
+    fig.update_layout(autosize=False,width=1000,height=600)
+    st.title("Distribution of Age")
+    st.plotly_chart(fig)
+
+    x = []
+    name = []
+    famous_sports = ['Basketball', 'Judo', 'Football', 'Tug-Of-War', 'Athletics',
+                     'Swimming', 'Badminton', 'Sailing', 'Gymnastics',
+                     'Art Competitions', 'Handball', 'Weightlifting', 'Wrestling',
+                     'Water Polo', 'Hockey', 'Rowing', 'Fencing',
+                     'Shooting', 'Boxing', 'Taekwondo', 'Cycling', 'Diving', 'Canoeing',
+                     'Tennis', 'Golf', 'Softball', 'Archery',
+                     'Volleyball', 'Synchronized Swimming', 'Table Tennis', 'Baseball',
+                     'Rhythmic Gymnastics', 'Rugby Sevens',
+                     'Beach Volleyball', 'Triathlon', 'Rugby', 'Polo', 'Ice Hockey']
+    for sport in famous_sports:
+        temp_df = athlete_df[athlete_df['Sport'] == sport]
+        x.append(temp_df[temp_df['Medal'] == 'Gold']['Age'].dropna())
+        name.append(sport)
+
+    fig = ff.create_distplot(x, name, show_hist=False, show_rug=False)
+    fig.update_layout(autosize=False, width=1000, height=600)
+    st.title("Distribution of Age wrt Sports(Gold Medalist)")
+    st.plotly_chart(fig)
+
+    sport_list = df['Sport'].unique().tolist()
+    sport_list.sort()
+    sport_list.insert(0, 'Overall')
+
+    st.title("Men Vs Women Participation Over the Years")
+    final = helper.men_vs_women(df)
+    fig = px.line(final, x="Year", y=["Male", "Female"])
+    fig.update_layout(autosize=False, width=1000, height=600)
+    st.plotly_chart(fig)
+
+if user_menu == 'News':
+    st.sidebar.header("News")
+    api_key = '07ce6431517e45c5b04b589c36e5bed6'
+    url = f"https://newsapi.org/v2/everything?q=olympics&apiKey={api_key}"
+    response = requests.get(url)
+    response = response.json()
+    articles = response['articles']
+
+    def get_news() :
+        for article in random.choices(articles) :
+            st.header(article['title'])
+            st.markdown(f"<span style='background-color:blue; padding:10px; border-radius:20px;'>Published at :"
+                        f"{article['publishedAt']}</span>",unsafe_allow_html=True)
+            if article['author'] :
+                st.write(article['author'])
+            st.write(article['source']['name'])
+            st.write(article['description'])
+            st.image(article['urlToImage'])
+        return
+
+    for i in range(5) :
+        get_news()
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    if col3.button('Refresh'):
+        st.experimental_rerun()
